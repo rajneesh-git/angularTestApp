1ad644eb96b3dbf4793f004b0844cbb22e8fd08f
Thakur Saab <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2017-11-30 22:04:09 -0500

    first commit

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector : 'about-app',
+  templateUrl : './about.component.html'
+})
+
+export class AboutComponent {
+description: string = "inside About component ts";
+
+}
